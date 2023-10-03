952f0367dd8767ad5918e85f97ffe955f5bcd09d
baksakcci <pshsh910@gmail.com>
2023-10-03 19:03:47 +0900

    [#2-1] refactor($AppTest) : 사용안한 import 제거

--- string-addition-calculator/app/src/test/java/personal/AppTest.java
+++ string-addition-calculator/app/src/test/java/personal/AppTest.java
@@ -1,12 +1,8 @@
 package personal;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
