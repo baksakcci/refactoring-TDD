da970028a3628dae9e86347e64bb33374bb70508
baksakcci <pshsh910@gmail.com>
2023-10-03 18:08:56 +0900

    [#1] test($AppTest): 입력 테스트 추가
    - System.setIn()을 활용하여 커스텀한 inputStream 등록하는 방법 사용
    - main에 입력 받고 출력하는 코드 추가

--- string-addition-calculator/app/src/main/java/personal/App.java
+++ string-addition-calculator/app/src/main/java/personal/App.java
@@ -1,7 +1,11 @@
 package personal;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(input);
     }
 }
--- string-addition-calculator/app/src/test/java/personal/AppTest.java
+++ string-addition-calculator/app/src/test/java/personal/AppTest.java
@@ -1,5 +1,21 @@
 package personal;
 
-class AppTest {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class AppTest {
+    private static final String input = "1+2";
+    @Test
+    @DisplayName("입력 테스트")
+    void input() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        Scanner scanner = new Scanner(System.in);
+        Assertions.assertThat(scanner.next()).isEqualTo("1+2");
+    }
 }
