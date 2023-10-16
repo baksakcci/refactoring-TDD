d546c5ba91d8c4e44d98f08e265d2ec1f781f8d0
baksakcci <pshsh910@gmail.com>
2023-10-17 03:40:19 +0900

    [#2] feat($PasswordStrengthTest): "비밀번호가 0부터 9까지 숫자를 포함하면 True 반환" 테스트 추가

--- testing-password-strength/app/src/test/java/testing/password/strength/Password.java
+++ testing-password-strength/app/src/test/java/testing/password/strength/Password.java
@@ -1,8 +1,13 @@
 package testing.password.strength;
 
-public class Password {
+import java.util.regex.Pattern;
 
+public class Password {
     public boolean verifyLength(String password) {
         return password.length() >= 8;
+    }
+
+    public boolean verifyContainsNumber(String password) {
+        return Pattern.matches("^.*[0-9].*$", password);
     }
 }
--- testing-password-strength/app/src/test/java/testing/password/strength/PasswordStrengthTest.java
+++ testing-password-strength/app/src/test/java/testing/password/strength/PasswordStrengthTest.java
@@ -11,4 +11,15 @@ public class PasswordStrengthTest {
         Assertions.assertThat(new Password().verifyLength("12345678"))
             .isTrue();
     }
+
+    @Test
+    @DisplayName("비밀번호가_0부터 9까지 숫자를 포함하면_True를 반환한다")
+    void verify_password_contains_number() {
+        Assertions.assertThat(new Password().verifyContainsNumber("1123"))
+            .isTrue();
+        Assertions.assertThat(new Password().verifyContainsNumber("a2bc"))
+            .isTrue();
+        Assertions.assertThat(new Password().verifyContainsNumber("3afdg6so9"))
+            .isTrue();
+    }
 }
