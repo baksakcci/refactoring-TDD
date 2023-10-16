ebf2b4072cfcabf1620fe3379db36bb951dc7364
baksakcci <pshsh910@gmail.com>
2023-10-17 03:48:08 +0900

    [#2] refactor($PasswordStrengthTest): Password 객체 생성 코드를 필드값으로 옮김

--- testing-password-strength/app/src/test/java/testing/password/strength/PasswordStrengthTest.java
+++ testing-password-strength/app/src/test/java/testing/password/strength/PasswordStrengthTest.java
@@ -5,21 +5,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthTest {
+
+    private Password password = new Password();
+
     @Test
     @DisplayName("비밀번호가_8글자 이상이면_True를 반환한다")
     void verify_password_length() {
-        Assertions.assertThat(new Password().verifyLength("12345678"))
+        Assertions.assertThat(password.verifyLength("12345678"))
             .isTrue();
     }
 
     @Test
     @DisplayName("비밀번호가_0부터 9까지 숫자를 포함하면_True를 반환한다")
     void verify_password_contains_number() {
-        Assertions.assertThat(new Password().verifyContainsNumber("1123"))
+        Assertions.assertThat(password.verifyContainsNumber("1123"))
             .isTrue();
-        Assertions.assertThat(new Password().verifyContainsNumber("a2bc"))
+        Assertions.assertThat(password.verifyContainsNumber("a2bc"))
             .isTrue();
-        Assertions.assertThat(new Password().verifyContainsNumber("3afdg6so9"))
+        Assertions.assertThat(password.verifyContainsNumber("3afdg6so9"))
             .isTrue();
     }
 }
