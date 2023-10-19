becaee95cdf1d89e339c6e36f20ab0a2cd89a270
baksakcci <pshsh910@gmail.com>
2023-10-19 20:04:43 +0900

    feat($test-module): sub module test
    - 잘 돌아가는지 체크 완료

---
+++ test-module/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        System.out.println(i);
+        System.out.println("Hello world!");
+    }
+}
---
+++ testing-password-strength/src/test/java/org/example/PasswordStrengthTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PasswordStrengthTest {
+
+    private Password password;
+
+    @BeforeEach
+    void beforeAll() {
+        password = new Password();
+    }
+
+//    @Test
+//    @DisplayName("비밀번호가_8글자 이상이면_True를 반환한다")
+//    void verify_password_length() {
+//        Assertions.assertThat(password.verifyLength("12345678"))
+//            .isTrue();
+//    }
+
+//    @Test
+//    @DisplayName("비밀번호가_0부터 9까지 숫자를 포함하면_True를 반환한다")
+//    void verify_password_contains_number() {
+//        Assertions.assertThat(password.verifyContainsNumber("1123"))
+//            .isTrue();
+//        Assertions.assertThat(password.verifyContainsNumber("a2bc"))
+//            .isTrue();
+//        Assertions.assertThat(password.verifyContainsNumber("3afdg6so9"))
+//            .isTrue();
+//    }
+
+//    @Test
+//    @DisplayName("비밀번호가_대문자를 포함하면_True를 반환한다")
+//    void verify_password_contains_uppercase() {
+//        Assertions.assertThat(password.verifyContainsUppercase("Ad24dfg90"))
+//            .isTrue();
+//    }
+
+    @Test
+    @DisplayName("비밀번호를 넣으면_검증과정을 거쳐_강도를 반환한다")
+    void verify_password_and_return_strength() {
+        Assertions.assertThat(password.verifyPassword("Tkdgur910"))
+            .isEqualTo(PasswordStrength.STRONG);
+        Assertions.assertThat(password.verifyPassword("asdwro324"))
+            .isEqualTo(PasswordStrength.NORMAL);
+        Assertions.assertThat(password.verifyPassword("910"))
+            .isEqualTo(PasswordStrength.WEAK);
+    }
+}
